433ff1138e2080c982722280edeafcea30fc5f0a
jpcarvallyo <jpcarvallyo@gmail.com>
2019-02-04 02:43:34 -0800

    Added README.md file and also modified Squares.js to handle content better in regards to styling. If user wanted to add more text to the content properties, they could without the styling looking off.

--- src/Squares.js
+++ src/Squares.js
@@ -7,8 +7,8 @@ class Squares extends React.Component {
       <div style={{height: '50vh'}}>
         <div style={{height: '100%', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
           <img style={{width: '2.5em'}} src={this.props.numIcon}/>
-          <h1 style={{color: 'black', fontSize: '3em', fontFamily: 'Roboto', marginTop: '0.4em', marginBottom: '0.4em'}}>{this.props.content}</h1>
-          <h2 style={{color: 'grey', fontWeight: '400', fontFamily: 'Roboto', margin: 0}}>{this.props.content2}</h2>
+          <h1 style={{color: 'black', fontSize: '3em', fontFamily: 'Roboto', marginTop: '0.4em', marginBottom: '0.4em', width: '80%', textAlign: 'center'}}>{this.props.content}</h1>
+          <h2 style={{color: 'grey', fontWeight: '400', fontFamily: 'Roboto', margin: 0, width: '80%', textAlign: 'center'}}>{this.props.content2}</h2>
         </div>
         <div style={{overflowX: 'hidden', overflowY: 'hidden', height: '50vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
           <img src={this.props.icon} style={{position: 'absolute', top: '73vh'}}/>
